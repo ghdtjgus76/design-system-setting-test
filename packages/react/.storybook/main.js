13eeadf49ca4c8815b53c90f81e7be58956f8509
ghdtjgus76 <ghdtjgus76@naver.com>
2024-04-13 14:57:20 +0900

    fix: react 워크 스페이스 초기 세팅 에러 해결

--- packages/react/.storybook/main.js
+++ packages/react/.storybook/main.js
@@ -1,15 +1,15 @@
-import type { StorybookConfig } from "@storybook/nextjs";
-
 import { join, dirname } from "path";
 
 /**
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
-function getAbsolutePath(value: string): any {
+function getAbsolutePath(value) {
   return dirname(require.resolve(join(value, "package.json")));
 }
-const config: StorybookConfig = {
+
+/** @type { import('@storybook/nextjs').StorybookConfig } */
+const config = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     getAbsolutePath("@storybook/addon-onboarding"),
@@ -25,6 +25,5 @@ const config: StorybookConfig = {
   docs: {
     autodocs: "tag",
   },
-  staticDirs: ["../public"],
 };
 export default config;
