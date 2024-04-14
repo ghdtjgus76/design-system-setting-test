ff88844905b8b5738dc4ac3c9080bb87ee1de1e3
ghdtjgus76 <ghdtjgus76@naver.com>
2024-04-14 14:32:05 +0900

    feat: styled-system 패키지 생성

---
+++ lerna-rollup-setting/packages/styled-system/types/index.d.ts
@@ -0,0 +1,7 @@
+/* eslint-disable */
+import './global'
+export * from './conditions';
+export * from './pattern';
+export * from './recipe';
+export * from './system-types';
+export * from './style-props';
